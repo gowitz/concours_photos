fb4bfa3eb642d59e1a48c8634a6003a3dd1c9037
gowitz <fab@ouranos.home>
2018-11-19 21:32:21 +0100

    finalisé projet

--- html/shuffle.py
+++ html/shuffle.py
@@ -2,7 +2,9 @@
 import random
 import string
 
-list = list(string.ascii_lowercase)
+#list = list(string.ascii_lowercase)
+list = range(01, 43)
 
+print ", ".join('%02d'%x for x in list)
 random.shuffle(list)
-print "Reshuffled list : ",  list
+print ", ".join('%02d'%x for x in list)
